23b88889760eeab4b97f9799a1be70f8aad6add1
Akim Savchenko <akim.savchenko@gmail.com>
2017-08-25 15:55:36 +0300

    Friday commit

--- getproducts.php
+++ getproducts.php
@@ -16,8 +16,9 @@ if(!empty($id)){
 }
 
 if(!$query = $db->query($sql)) {
+    
     json_encode(['status'=>'Error', 'msg'=>'Query failed']);
-        die('Connection to the Database failed!!!');
+        die('Connection to the Database failed when get products!!!');
 }
     $products = [];
     while($row = $query->fetch_assoc()) {
@@ -29,3 +30,31 @@ echo json_encode($products);
 /*1. bind_param
 1.5 убрать die рядом с json_encode, перед json_encode добавить echo (smiley)
 2. повставлять заголовок с НТТР статусом и кодом ошибки, где надо*/
+
+
+if(!empty($id)) {
+
+    // $sql = "UPDATE products SET title='{$title}', description='{$description}', price='{$price}'  WHERE id='{$id}'";
+    $sql = "UPDATE products SET title=?, description=?, price=?  WHERE id=?";
+    // In case id not empty update the existing record
+
+} else {
+    // In case id is empty, create a new record
+    $sql = "INSERT INTO products (title, price, description) VALUES (?, ?, ?)";
+}
+
+if(!empty($title) && !empty($price) && !empty($description)){
+
+   if($stmt = $db->mysqli()){
+
+       $stmt = $db->mysqli()->prepare($sql);
+       $stmt->bind_param("sds", $title, $price, $description);
+       $stmt->execute();
+
+       echo json_encode([ "status" => "ok", "msg" => "ok" ]);
+    }
+
+    
+}else{
+    echo json_encode([ "status" => "error", "msg" => "Complete all required fields" ]);
+}
--- js/main.js
+++ js/main.js
@@ -1,58 +1,32 @@
-
-function appendProduct (obj) {
-    $('.product-list > .row').append("<div class='col-md-4' data-id='" +
-        obj.id + "'><h2>" + obj.title + "</h2><p>" + obj.price + "</p><p>" +
-        obj.description + "</p><button class='btn btn-success editProduct' " +
-        "data-toggle='modal' data-target='#myModal'>Edit</button>&nbsp; &nbsp;<button class='btn btn-danger removeProduct'>Delete</button></div></div>" );
+function appendProduct(obj) {
+    $('.product-list > .row').append("<div class='col-md-4' data-id='" + obj.id + "'><h2>" + obj.title + "</h2><p>" + obj.price + "</p><p>" + obj.description + "</p><button class='btn btn-success editProduct' data-toggle='modal' data-target='#myModal'>Edit</button>&nbsp; &nbsp;<button class='btn btn-danger removeProduct'>Delete</button></div></div>" );
 }
 
-    $.get('/getproducts.php', function(res){
-        console.log(res);
-        $.each(res, function(i, obj){
+function getProducts() {
+    $('.product-list > .row > div').remove();
+    $.get('/getproducts.php', function(res) {
+        $.each(res, function (i,obj){
             appendProduct(obj);
         });
     });
-
-
-//function getProducts (){
-//
-//    $.get('/getproducts.php', function(res){
-//        console.log(res);
-//        $.each(res, function(i, obj){
-//            appendProduct(obj);
-//        });
-//
-//        // $('.product-list > .row > div').remove();
-//
-//    });
-//
-//}
+}
 
 $('.newproduct').click(function () {
-
-
-    $.get('/addnewproduct.php?', function(res){
-        var item = res[0];
-        $('#title').val(item.title);
-        $('#price').val(item.price);
-        $('#description').val(item.description);
-    });
-
     
     var product = {
-        id: $('.product-id').val(),
+        id: $('.productID').val(),
         title: $('#title').val(),
         price: $('#price').val(),
         description: $('#description').val()
     };
-    // console.log(product.id, typeof product.id);
-
+    
     $.post('/addnewproduct.php', product, function (res) {
         console.log(res);
-        if(res.status == "ok" && product.id == ""){
+        
+        if(res.status == "ok" && product.id == "") {
             appendProduct(product);
         }
-        // getProducts();
+        getProducts();
     });
     
     $('#title').val('');
@@ -63,6 +37,7 @@ $('.newproduct').click(function () {
 });
 
 $('.addProduct').click(function () {
+    
     $('#title').val('');
     $('#price').val('');
     $('#description').val('');
@@ -72,27 +47,32 @@ $('.addProduct').click(function () {
 
 
 $(document).on('click', '.removeProduct', function () {
+    
     var itemId = $(this).parent().attr('data-id');
-
-    if(confirm("Вы действительно хотите удалить?")){
-        // sessionStorage.removeItem(itemId);
-        $.post('/removeproducts.php', {id:itemId}, function (result) {
-            // console.log(itemId);
+    $('.productID').val(itemId);
+    
+    if(confirm("Вы действительно хотите удалить?")) {
+        
+        $.post('/removeproducts.php', {id:itemId}, function(res) {
+            
         });
+        
         $(this).parent().remove();
     }
 });
 
 $(document).on('click', '.editProduct', function () {
     var itemId = $(this).parent().attr('data-id');
-    $(".product-id").val(itemId);
-
-    $.get('/getproducts.php?id='+itemId, function(res){
-       var item = res[0];
-        $('#title').val(item.title);
-        $('#price').val(item.price);
-        $('#description').val(item.description);
-    });
-
+    $('.productID').val(itemId);
+    
+    $.get('/getproducts.php?id='+itemId, function (res) {
+		var item = res[0];
+		$('#title').val(item.title);
+    	$('#price').val(item.price);
+    	$('#description').val(item.description);
+	});
+    
     $('.newproduct').attr('data-id', itemId);
 });
+
+getProducts();
